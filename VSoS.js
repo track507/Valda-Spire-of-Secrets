0925b385cfdca7398e44cae25cd36408cf1bf187
track507 <136283573+track507@users.noreply.github.com>
2024-09-23 02:27:06 -0500

    Final "G" push

--- VSoS.js
+++ VSoS.js
@@ -33171,7 +33171,7 @@ SpellsList["grasp of the grave"] = {
 	save : "Str",
 	ritual: false,
     description : "20-ft rad becomes diff. terrain; crea entering or starting turn save or 3d6+1d6/SL Necro dmg; B",
-	descriptionShorter : "20 ft rad dif ter; Non-undead enter/SoT/A save or 3d6+1d6/SL Necro, move in rad; save \u00BD, no move",
+	descriptionShorter : "20 ft rad dif ter; Non-undead enter/SoT/A save or 3d6+1d6/SL Necro, move w/in rad; save \u00BD, no move",
 	descriptionFull : "Spectral skeletal arms emerge from the ground in a 20-foot-radius circle, centered on a point you can see within range. The arms grasp at creatures within the area, except for undead creatures, which ignore the effects of this spell. The area is difficult terrain." + "\n   " + "When a creature enters the area for the first time on a turn or starts its turn there, it must make a Strength saving throw. On a failed save, a creature takes 3d6 necrotic damage, and is dragged by the hands to any unoccupied space within the area that you choose. On a successful save, a creature takes half as much damage and is not moved." + "\n   " + "Until the spell ends, you can use your action to force each creature within the area to immediately make a save against this spell." + AtHigherLevels + "When you cast this spell using a spell slot of 5th level or higher, its damage increases by 1d6 for each slot level above 4th."
 };
 SpellsList["guided missile"] = {
